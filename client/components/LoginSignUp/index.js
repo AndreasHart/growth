287449ddc3864ae75045a448dafc762432336136
Andreas Hart <andreastheohart@gmail.com>
2017-10-25 19:04:36 -0300

    finishing the login/signup form

--- client/components/LoginSignUp/index.js
+++ client/components/LoginSignUp/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { IndexLink } from 'react-router';
-import { inputBox, input, box } from './styles';
+import { inputBox, buttonBox, input, title, box, button, titleBox } from './styles';
 import { toggleLoginSignup } from '../../actions';
 
 class LoginSignUp extends Component {
@@ -18,21 +18,38 @@ class LoginSignUp extends Component {
   render() {
     const { loginOrSignUp } = this.props;
     if(loginOrSignUp){
-      return <div className={box} >
-      <Helmet title='Login' />
-      <h2 >Login</h2>
-      <div>No account? <a onClick={this.handleToggleLogin} >SignUp</a></div>
-      <div className={inputBox}>
-        <input className={input} type='text' placeholder={'Email'} />
-        <input className={input} type='password' placeholder={'Password'} />
-      </div>
-    </div>;
+      return (
+        <div className={box} >
+          <div className={titleBox}>
+            <Helmet title='Login' />
+            <h2 className={title}>Login</h2>
+            <div>No account? <a onClick={this.handleToggleLogin} >SignUp</a></div>
+          </div>
+          <div className={inputBox}>
+            <input className={input} type='text' placeholder={'Email'} />
+            <input className={input} type='password' placeholder={'Password'} />
+          </div>
+          <div className={buttonBox}>
+            <a className={button} >Login</a>
+          </div>
+        </div>)
   } else {
-    return <div >
-      <Helmet title='SignUp' />
-      <h2 >Sign Up</h2>
-      <div>Have account? <a onClick={this.handleToggleLogin} >Login</a></div>
-    </div>;
+    return (
+      <div className={box} >
+        <Helmet title='SignUp' />
+        <div className={titleBox}>
+          <h2  className={title}>Sign Up</h2>
+          <div>Have account? <a onClick={this.handleToggleLogin} >Login</a></div>
+        </div>
+        <div className={inputBox}>
+          <input className={input} type='text' placeholder={'Email'} />
+          <input className={input} type='password' placeholder={'Password'} />
+          <input className={input} type='password' placeholder={'Password Again Pls'} />
+        </div>
+        <div className={buttonBox}>
+          <a className={button} >Sign Up</a>
+        </div>
+      </div>);
 
   }
   }
